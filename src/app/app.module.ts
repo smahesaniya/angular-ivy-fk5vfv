fa7a86e27c5981cdefd673af9b34a10ed8e2bb1f
smahesaniya <sajid13@gmail.com>
2020-10-28 15:56:17 -0400

    2 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TripEditComponent } from './components/trip-edit/trip-edit.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes), ],
+  declarations: [ AppComponent, HelloComponent,
+    TripEditComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
